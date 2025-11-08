82523b6449f9696ead3b2d2ec8e62838dff7eb8c
DAIGOSHIROMA <daigo-siroma-k23c@stu.kbc.ac.jp>
2025-11-08 15:48:13 +0900

    searchページをdynamicに変更

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,6 @@
 // src/app/search/page.tsx
 
-"use client";
-
-export const dynamic = 'force-dynamic';
+'use client';
 
 import { Suspense, useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
